7192c7083d0c7eb7f2a4f75c5a10f63de161ae8c
wamoonsky7414 <123857589+wamoonsky7414@users.noreply.github.com>
2024-03-18 22:39:35 +0800

    push la
    push la

--- backtest/operators.py
+++ backtest/operators.py
@@ -127,54 +127,309 @@ def ts_kurtosis(df: pd.DataFrame,period:int) -> pd.DataFrame:
     return rolling_kurtosis
 
 
+# def rolling_beta(df: pd.DataFrame, index_series:pd.Series ,period:int) -> pd.DataFrame:
+
+#     def ols_regression(X, Y):
+#         X = np.column_stack((np.ones(X.shape[0]), X))
+#         beta = np.linalg.inv(X.T @ X) @ X.T @ Y
+        
+#         return beta[1:]
+
+#     def calculate_betas(stock_returns, index_series, window_length):
+#         betas = []
+#         indices = []  # 用於存儲對應的索引
+
+#         for i in range(window_length, len(stock_returns) + 1):
+#             X = index_series[i-window_length:i].values.reshape(-1, 1)
+#             Y = stock_returns[i-window_length:i].values
+#             beta = ols_regression(X, Y)
+#             betas.append(beta.item())  # 添加斜率系数
+#             indices.append(stock_returns.index[i-1])  # 添加对应的索引
+            
+#         return pd.Series(betas, index=indices)
 
 
-def rolling_beta(df: pd.DataFrame, index_series:pd.Series ,period:int) -> pd.DataFrame:
+#     market_ret = index_series.dropna()
+#     beta_df = pd.DataFrame()
+#     deleted_columns = []
 
+#     for target in df.columns:
+#         stock_ret = df[target]
+#         stock_ret.index = pd.to_datetime(stock_ret.index)
+#         if stock_ret.empty:
+#             deleted_columns.append(target)
+#         else:
+
+#             stock_first_index = stock_ret.index[0]
+#             market_first_index = market_ret.index[0]
+#             stock_last_index = stock_ret.index[-1]
+#             market_last_index = market_ret.index[-1]
+            
+#             common_start_date = max(stock_first_index, market_first_index)
+#             common_end_date = min(stock_last_index, market_last_index)
+#             stock_ret = stock_ret[common_start_date:common_end_date]
+#             M_ret = market_ret.loc[common_start_date:common_end_date]  
+
+#             betas = calculate_betas(stock_ret, M_ret, period)
+#             betas = pd.DataFrame(betas)
+#             betas.columns = [target]
+#             beta_df = pd.concat([beta_df, betas], axis=1)
+    
+#     return beta_df
+
+
+# def rolling_alpha(df: pd.DataFrame, index_series:pd.Series ,period:int) -> pd.DataFrame:
+
+#     def ols_regression(X, Y):
+#         # 為 X 增加常數列
+#         X = np.column_stack((np.ones(X.shape[0]), X))
+        
+#         # 使用公式計算係數：(X'X)^(-1)X'Y
+#         beta = np.linalg.inv(X.T @ X) @ X.T @ Y
+
+#         # 分離截距項和斜率係數
+#         alpha = beta[:1]
+#         coefficients = beta[1:]
+#         return alpha, coefficients
+
+#     def calculate_residuals(stock_prices, index_prices, window_length):
+#         residuals = []
+#         indices = []  # 用於存儲對應的索引
+#         # alpha_index = stock_prices.index
+
+#         # 使用指定的滾動窗口長度
+#         for i in range(window_length, len(stock_prices)):
+#             X = index_prices[i-window_length:i]
+#             y = stock_prices[i-window_length:i]
+#             alpha, beta = ols_regression(X.values.reshape(-1, 1), y.values)
+#             alpha_series = pd.Series([alpha] * len(y), index=y.index)
+#             predicted_y = X * beta
+#             residual = y.iloc[-1] - predicted_y.iloc[-1] - alpha_series.iloc[-1]
+#             residuals.append(residual)
+#             indices.append(y.index[-1])  # 添加對應的索引
+            
+#         return pd.Series(residuals, index=indices)
+    
+#     market_ret = index_series.dropna()
+#     beta_df = pd.DataFrame()
+#     deleted_columns = []
+
+#     for target in df.columns:
+#         stock_ret = df[target]
+#         stock_ret.index = pd.to_datetime(stock_ret.index)
+#         if stock_ret.empty:
+#             deleted_columns.append(target)
+#         else:
+
+#             stock_first_index = stock_ret.index[0]
+#             market_first_index = market_ret.index[0]
+#             stock_last_index = stock_ret.index[-1]
+#             market_last_index = market_ret.index[-1]
+            
+#             common_start_date = max(stock_first_index, market_first_index)
+#             common_end_date = min(stock_last_index, market_last_index)
+#             stock_ret = stock_ret[common_start_date:common_end_date]
+#             M_ret = market_ret.loc[common_start_date:common_end_date]  
+
+#             alphas = calculate_residuals(stock_ret, M_ret, period)
+#             alphas = pd.DataFrame(alphas)
+#             alphas.columns = [target]
+#             alpha_df = pd.concat([beta_df, alphas], axis=1)
+    
+#     return alpha_df
+
+
+# def rolling_regression(df: pd.DataFrame, index_series: pd.Series, period: int, return_type: str = 'alpha') -> pd.DataFrame:
+#     def ols_regression(X, Y):
+#         X = np.column_stack((np.ones(X.shape[0]), X))
+#         beta = np.linalg.inv(X.T @ X) @ X.T @ Y
+#         alpha = beta[:1]
+#         coefficients = beta[1:]
+#         return alpha, coefficients
+
+#     def calculate_values(stock_prices, index_prices, window_length, return_type):
+#         values = []
+#         indices = []
+
+#         for i in range(window_length, len(stock_prices)):
+#             X = index_prices[i-window_length:i]#.values.reshape(-1, 1)
+#             y = stock_prices[i-window_length:i]#.values
+#             alpha, beta = ols_regression(X.values.reshape(-1, 1), y.values)
+
+#             if return_type == 'alpha':
+#                 alpha_series = pd.Series([alpha] * len(y), index=y.index)
+#                 predicted_y = X * beta
+#                 residual = y.iloc[-1] - predicted_y.iloc[-1] - alpha_series.iloc[-1]
+#                 values.append(residual)
+
+#             elif return_type == 'beta':
+#                 values.append(beta)
+#             indices.append(y.index[-1])
+
+#         return pd.Series(values, index=indices)
+    
+#     market_ret = index_series.dropna()
+#     columns_df = pd.DataFrame()
+
+#     for target in df.columns:
+#         stock_ret = df[target].dropna()
+#         if stock_ret.empty:
+#             continue
+
+#         stock_ret.index = pd.to_datetime(stock_ret.index)
+#         market_ret.index = pd.to_datetime(market_ret.index)
+
+#         stock_first_index = stock_ret.index[0]
+#         market_first_index = market_ret.index[0]
+#         stock_last_index = stock_ret.index[-1]
+#         market_last_index = market_ret.index[-1]
+        
+#         common_start_date = max(stock_first_index, market_first_index)
+#         common_end_date = min(stock_last_index, market_last_index)
+#         stock_ret = stock_ret[common_start_date:common_end_date]
+#         M_ret = market_ret.loc[common_start_date:common_end_date]  
+
+
+#         values = calculate_values(stock_ret, M_ret, period, return_type)
+#         values = pd.DataFrame(values)
+#         values.columns = [target]
+#         print(values)
+#         columns_df = pd.concat([columns_df, values], axis=1)
+#     columns_df = columns_df.applymap(lambda x: x[0] if isinstance(x, np.ndarray) else x)
+
+#     return columns_df
+
+
+def rolling_regression(regression_y: pd.DataFrame, regression_x: pd.Series | pd.DataFrame, period: int, return_type: str = 'alpha') -> pd.DataFrame:
     def ols_regression(X, Y):
         X = np.column_stack((np.ones(X.shape[0]), X))
         beta = np.linalg.inv(X.T @ X) @ X.T @ Y
-        
-        return beta[1:]
+        alpha = beta[:1]
+        coefficients = beta[1:]
+        return alpha, coefficients
 
-    def calculate_betas(stock_returns, index_series, window_length):
-        betas = []
-        indices = []  # 用於存儲對應的索引
+    def calculate_values(stock_prices, index_prices, window_length, return_type):
+        values = []
+        indices = []
 
-        for i in range(window_length, len(stock_returns) + 1):
-            X = index_series[i-window_length:i].values.reshape(-1, 1)
-            Y = stock_returns[i-window_length:i].values
-            beta = ols_regression(X, Y)
-            betas.append(beta.item())  # 添加斜率系数
-            indices.append(stock_returns.index[i-1])  # 添加对应的索引
-            
-        return pd.Series(betas, index=indices)
+        for i in range(window_length, len(stock_prices)):
+            X = index_prices[i-window_length:i]#.values.reshape(-1, 1)
+            y = stock_prices[i-window_length:i]#.values
+            alpha, beta = ols_regression(X.values.reshape(-1, 1), y.values)
 
+            if return_type == 'alpha':
+                alpha_series = pd.Series([alpha] * len(y), index=y.index)
+                predicted_y = X * beta
+                residual = y.iloc[-1] - predicted_y.iloc[-1] - alpha_series.iloc[-1]
+                values.append(residual)
 
-    market_ret = index_series.dropna()
-    beta_df = pd.DataFrame()
-    deleted_columns = []
+            elif return_type == 'beta':
+                values.append(beta)
+            indices.append(y.index[-1])
+
+        return pd.Series(values, index=indices)
+    
+    market_ret = regression_x.dropna()
+    columns_df = pd.DataFrame()
 
     for target in df.columns:
-        stock_ret = df[target]
-        stock_ret.index = pd.to_datetime(stock_ret.index)
+        stock_ret = df[target].dropna()
         if stock_ret.empty:
-            deleted_columns.append(target)
-        else:
+            continue
 
-            stock_first_index = stock_ret.index[0]
-            market_first_index = market_ret.index[0]
-            stock_last_index = stock_ret.index[-1]
-            market_last_index = market_ret.index[-1]
-            
-            common_start_date = max(stock_first_index, market_first_index)
-            common_end_date = min(stock_last_index, market_last_index)
-            stock_ret = stock_ret[common_start_date:common_end_date]
-            M_ret = market_ret.loc[common_start_date:common_end_date]  
+        stock_ret.index = pd.to_datetime(stock_ret.index)
+        market_ret.index = pd.to_datetime(market_ret.index)
 
-            betas = calculate_betas(stock_ret, M_ret, period)
-            betas = pd.DataFrame(betas)
-            betas.columns = [target]
-            beta_df = pd.concat([beta_df, betas], axis=1)
+        stock_first_index = stock_ret.index[0]
+        market_first_index = market_ret.index[0]
+        stock_last_index = stock_ret.index[-1]
+        market_last_index = market_ret.index[-1]
+        
+        common_start_date = max(stock_first_index, market_first_index)
+        common_end_date = min(stock_last_index, market_last_index)
+        stock_ret = stock_ret[common_start_date:common_end_date]
+        M_ret = market_ret.loc[common_start_date:common_end_date]  
+
+
+        values = calculate_values(stock_ret, M_ret, period, return_type)
+        values = pd.DataFrame(values)
+        values.columns = [target]
+        print(values)
+        columns_df = pd.concat([columns_df, values], axis=1)
+    columns_df = columns_df.applymap(lambda x: x[0] if isinstance(x, np.ndarray) else x)
+
+    return columns_df
+
+
+
+
+# def rolling_regression(df: pd.DataFrame, index_series: pd.Series | pd.DataFrame, period: int, return_type: str = 'alpha') -> pd.DataFrame:
+#     def ols_regression(X, Y):
+#         X = np.column_stack((np.ones(X.shape[0]), X))
+#         beta = np.linalg.inv(X.T @ X) @ X.T @ Y
+#         alpha = beta[0]
+#         coefficients = beta[1:]
+#         return alpha, coefficients
+
+#     def calculate_values(stock_prices, index_prices, window_length, return_type):
+#         values = []
+#         indices = []
+
+#         for i in range(window_length, len(stock_prices)):
+#             X = index_prices[i-window_length:i]
+#             y = stock_prices[i-window_length:i]
+#             alpha, beta = ols_regression(X.values.reshape(-1, 1), y.values)
+
+#             if return_type == 'alpha':
+#                 alpha_series = pd.Series([alpha] * len(y), index=y.index)
+#                 predicted_y = X * beta + alpha
+#                 residual = y.iloc[-1] - predicted_y.iloc[-1]
+#                 values.append(residual)
+
+#             elif return_type == 'beta':
+#                 values.append(beta)
+#             indices.append(y.index[-1])
+
+#         return pd.Series(values, index=indices)
     
-    return beta_df
+#     columns_df = pd.DataFrame()
+
+#     if isinstance(index_series, pd.DataFrame):
+#         for target in df.columns:
+#             if target in index_series.columns:
+#                 market_ret = index_series[target].dropna()
+#                 stock_ret = df[target].dropna()
+#                 if stock_ret.empty or market_ret.empty:
+#                     continue
+
+#                 stock_ret.index = pd.to_datetime(stock_ret.index)
+#                 market_ret.index = pd.to_datetime(market_ret.index)
+
+#                 common_start_date = max(stock_ret.index[0], market_ret.index[0])
+#                 common_end_date = min(stock_ret.index[-1], market_ret.index[-1])
+
+#                 stock_ret = stock_ret[common_start_date:common_end_date]
+#                 market_ret = market_ret[common_start_date:common_end_date]
+
+#                 values = calculate_values(stock_ret, market_ret, period, return_type)
+#                 columns_df[target] = values
+#     else:
+#         market_ret = index_series.dropna()
+#         for target in df.columns:
+#             stock_ret = df[target].dropna()
+#             if stock_ret.empty:
+#                 continue
+
+#             stock_ret.index = pd.to_datetime(stock_ret.index)
+#             market_ret.index = pd.to_datetime(market_ret.index)
+
+#             common_start_date = max(stock_ret.index[0], market_ret.index[0])
+#             common_end_date = min(stock_ret.index[-1], market_ret.index[-1])
+
+#             stock_ret = stock_ret[common_start_date:common_end_date]
+#             market_ret = market_ret[common_start_date:common_end_date]
+
+#             values = calculate_values(stock_ret, market_ret, period, return_type)
+#             columns_df[target] = values
+
+#     return columns_df
