74d0286d511441675470341b357747149a862420
wamoonsky7414 <123857589+wamoonsky7414@users.noreply.github.com>
2024-04-03 17:18:12 +0800

    update
    update

--- data_center/tej_handler.py
+++ data_center/tej_handler.py
@@ -3,11 +3,15 @@ import json
 import pandas as pd
 from IPython.display import display
 import tejapi
+import sys
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(1,PROJECT_ROOT)
 
-class TEJHandler(object):
+
+class TEJHandler():
     def __init__(self):
+        super().__init__()
         self.data_center_config = self.get_data_center_config()
 
         self.api_key = self.data_center_config['api_key']
@@ -37,77 +41,77 @@ class TEJHandler(object):
         df = pd.DataFrame([self.tej_data_table]).T
         display(df)
 
-    # ======================== arrange raw data to Pdata ================ #
+    # # ======================== arrange raw data to Pdata ================ #
 
-    def pdata_pipline(self, df: pd.DataFrame, data_code: str):
-        df = self.rename_symbol_and_datetime(df, data_code)
-        df_dict = self.unstack_data(df, data_code)
-        return df_dict 
+    # def pdata_pipline(self, df: pd.DataFrame, data_code: str):
+    #     df = self.rename_symbol_and_datetime(df, data_code)
+    #     df_dict = self.unstack_data(df, data_code)
+    #     return df_dict 
 
-    def rename_symbol_and_datetime(self, df: pd.DataFrame, data_code: str):
-        if data_code in ["TWN/EWGIN", "TWN/EWPRCD", "TWN/EWPRCD2", "TWN/EWTINST1"]:
-            df.rename(columns={'證券碼': 'symbol', '日期': 'datetime'}, inplace=True)
-        elif data_code == "TWN/EWSALE":
-            df.rename(columns={'證券碼': 'symbol', '資料日期': 'datetime'}, inplace=True)
-        elif data_code == "TWN/EWIFINQ":
-            df.rename(columns={'證券碼': 'symbol', '財務資料日': 'datetime'}, inplace=True)
-        elif data_code == "TWN/EWIPRCD":
-            df.rename(columns={'指數碼': 'symbol', '日期': 'datetime'}, inplace=True)
-        elif data_code == "TWN/EWIPRCSTD":
-            df.rename(columns={'指數碼': 'symbol', '目前狀態': 'datetime'}, inplace=True)
-        elif data_code == "TWN/EWNPRCSTD":
-            df.rename(columns={'證券碼': 'symbol', '目前狀態': 'datetime'}, inplace=True)
-        else:
-            print("Unsupported data code. Please use one of the following data codes with the correct column names to rename:")
-            print(self.tej_data_table)
-        return df
+    # def rename_symbol_and_datetime(self, df: pd.DataFrame, data_code: str):
+    #     if data_code in ["TWN/EWGIN", "TWN/EWPRCD", "TWN/EWPRCD2", "TWN/EWTINST1"]:
+    #         df.rename(columns={'證券碼': 'symbol', '日期': 'datetime'}, inplace=True)
+    #     elif data_code == "TWN/EWSALE":
+    #         df.rename(columns={'證券碼': 'symbol', '資料日期': 'datetime'}, inplace=True)
+    #     elif data_code == "TWN/EWIFINQ":
+    #         df.rename(columns={'證券碼': 'symbol', '財務資料日': 'datetime'}, inplace=True)
+    #     elif data_code == "TWN/EWIPRCD":
+    #         df.rename(columns={'指數碼': 'symbol', '日期': 'datetime'}, inplace=True)
+    #     elif data_code == "TWN/EWIPRCSTD":
+    #         df.rename(columns={'指數碼': 'symbol', '目前狀態': 'datetime'}, inplace=True)
+    #     elif data_code == "TWN/EWNPRCSTD":
+    #         df.rename(columns={'證券碼': 'symbol', '目前狀態': 'datetime'}, inplace=True)
+    #     else:
+    #         print("Unsupported data code. Please use one of the following data codes with the correct column names to rename:")
+    #         print(self.tej_data_table)
+    #     return df
 
-    def unstack_data(self, df: pd.DataFrame, data_code: str):
-        df_dic = {}
-        column_list = df.columns.to_list()
-        for column in column_list[3:]:
-            unstack_df = df.reset_index().set_index(['datetime', 'symbol'])[column].unstack()
-            df_dic[column] = unstack_df
-        return df_dic
+    # def unstack_data(self, df: pd.DataFrame, data_code: str):
+    #     df_dic = {}
+    #     column_list = df.columns.to_list()
+    #     for column in column_list[3:]:
+    #         unstack_df = df.reset_index().set_index(['datetime', 'symbol'])[column].unstack()
+    #         df_dic[column] = unstack_df
+    #     return df_dic
     
-    # =========================== new function after 2024/03/27 ====================== #
+    # # =========================== new function after 2024/03/27 ====================== #
 
-    def get_daily_frequent(self, source_dict: dict, data_code: str, signal_delay: int= None):
+    # def get_daily_frequent(self, source_dict: dict, data_code: str, signal_delay: int= None):
         
-        factor_dict = {}
+    #     factor_dict = {}
 
-        date_df_code = "TWN/EWIPRCD"
-        date_df = tejapi.get(date_df_code, chinese_column_name=True,paginate=True)
-        for_date_dict = self.pdata_pipline(date_df, date_df_code)
+    #     date_df_code = "TWN/EWIPRCD"
+    #     date_df = tejapi.get(date_df_code, chinese_column_name=True,paginate=True)
+    #     for_date_dict = self.pdata_pipline(date_df, date_df_code)
 
-        if data_code == "TWN/EWSALE":
-            release = '營收發布日'
-        elif data_code == "TWN/EWIFINQ":
-            release = '財報發布日'
-        elif data_code in ["TWN/EWIPRCSTD", "TWN/EWNPRCSTD"]:
-            for column in list(source_dict.keys()):
-                data_reindex = source_dict[column].reindex(for_date_dict['指數收盤價'].index, method = 'ffill')
-                factor_dict[column] = data_reindex 
-            return factor_dict
+    #     if data_code == "TWN/EWSALE":
+    #         release = '營收發布日'
+    #     elif data_code == "TWN/EWIFINQ":
+    #         release = '財報發布日'
+    #     elif data_code in ["TWN/EWIPRCSTD", "TWN/EWNPRCSTD"]:
+    #         for column in list(source_dict.keys()):
+    #             data_reindex = source_dict[column].reindex(for_date_dict['指數收盤價'].index, method = 'ffill')
+    #             factor_dict[column] = data_reindex 
+    #         return factor_dict
         
-        for column in list(source_dict.keys()):
-            releaserankpct = source_dict[release].rank(axis=1,pct=True)
-            filter = source_dict[column][releaserankpct < signal_delay]
-            filter.index = source_dict[release][releaserankpct < signal_delay].max(axis=1)
-            data_reindex = filter.reindex(for_date_dict['指數收盤價'].index, method = 'ffill')
-            factor_dict[column] = data_reindex 
-        return factor_dict
+    #     for column in list(source_dict.keys()):
+    #         releaserankpct = source_dict[release].rank(axis=1,pct=True)
+    #         filter = source_dict[column][releaserankpct < signal_delay]
+    #         filter.index = source_dict[release][releaserankpct < signal_delay].max(axis=1)
+    #         data_reindex = filter.reindex(for_date_dict['指數收盤價'].index, method = 'ffill')
+    #         factor_dict[column] = data_reindex 
+    #     return factor_dict
 
-    def pmart_pipline(self, source_dict: dict, data_code: str, signal_delay: int= None):
+    # def pmart_pipline(self, source_dict: dict, data_code: str, signal_delay: int= None):
 
-        if type(signal_delay) == int:
-            signal_delay *=  0.01    
+    #     if type(signal_delay) == int:
+    #         signal_delay *=  0.01    
 
-        if data_code in ["TWN/EWGIN", "TWN/EWPRCD", "TWN/EWPRCD2", "TWN/EWTINST1", "TWN/EWIPRCD"]:
-            factor_dict = source_dict
-        elif data_code in ["TWN/EWSALE", "TWN/EWIFINQ", "TWN/EWIPRCSTD", "TWN/EWNPRCSTD"]:
-            factor_dict = self.get_daily_frequent(source_dict, data_code, signal_delay)
-        else:
-            print("Unsupported data code. Please use one of the following data codes with the correct column names to rename:")
-            print(self.tej_data_table)
-        return factor_dict
+    #     if data_code in ["TWN/EWGIN", "TWN/EWPRCD", "TWN/EWPRCD2", "TWN/EWTINST1", "TWN/EWIPRCD"]:
+    #         factor_dict = source_dict
+    #     elif data_code in ["TWN/EWSALE", "TWN/EWIFINQ", "TWN/EWIPRCSTD", "TWN/EWNPRCSTD"]:
+    #         factor_dict = self.get_daily_frequent(source_dict, data_code, signal_delay)
+    #     else:
+    #         print("Unsupported data code. Please use one of the following data codes with the correct column names to rename:")
+    #         print(self.tej_data_table)
+    #     return factor_dict
--- monitor/tw_monitor.py
+++ monitor/tw_monitor.py
@@ -8,7 +8,10 @@ from dateutil.relativedelta import relativedelta
 
 from pymongo import MongoClient
 
+import sys
+
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(1,PROJECT_ROOT)
 
 from data_center.tej_handler import TEJHandler
 
@@ -40,7 +43,7 @@ class TWMarketMonitor(TEJHandler):
         # 根据集合名决定排序字段
         if collection_str == "financial_report":
             sort_target = '財務資料日'
-        elif collection_str in ["margin_trading", "three_major_investors_activity", "securities_trading_data", "securities_returns"]:
+        elif collection_str in ["margin_trading", "three_major_investors_activity", "securities_trading_data", "securities_returns", "tsx"]:
             sort_target = '日期'
         elif collection_str == "monthly_revenue":
             sort_target = '資料日期'
@@ -50,24 +53,91 @@ class TWMarketMonitor(TEJHandler):
         df = pd.DataFrame(list(collection.find().sort(sort_target, -1).limit(limit)))
         return df
 
-    # ========================= get TEJ API information ===================== #
+    
+        # ======================== arrange raw data to Pdata ================ #
 
-    def get_market_returns(self, review_month: int=1):
-        today = date.today()
-        last_month_date = today - relativedelta(months=review_month)
-        last_month_date_str = last_month_date.strftime('%Y-%m-%d')
+    def pdata_pipline(self, df: pd.DataFrame, collection_str:str = 'financial_report'):
+        df = self.rename_symbol_and_datetime(df, collection_str)
+        df_dict = self.unstack_data(df)
+        return df_dict 
 
-        source_code = "TWN/EWPRCD2"
-        raw_data = tejapi.get(source_code, 
-                              chinese_column_name=True, 
-                              mdate={'gt':last_month_date_str},
-                              paginate=True)
-        pdata_df = TEJHandler().pdata_pipline(raw_data, source_code)
-        pmart = TEJHandler().pmart_pipline(pdata_df, source_code)
+    def rename_symbol_and_datetime(self, df: pd.DataFrame, collection_str: str):
+        if collection_str not in self.colletion_list:
+            raise ValueError(f"please use one of {self.colletion_list} in collection_str")
+        
+        if collection_str in ["margin_trading", "securities_trading_data", "securities_returns", "three_major_investors_activity"]:
+            df.rename(columns={'證券碼': 'symbol', '日期': 'datetime'}, inplace=True)
+        elif collection_str == "monthly_revenue":
+            df.rename(columns={'證券碼': 'symbol', '資料日期': 'datetime'}, inplace=True)
+        elif collection_str == "financial_report":
+            df.rename(columns={'證券碼': 'symbol', '財務資料日': 'datetime'}, inplace=True)
+        elif collection_str == "tsx":
+            df.rename(columns={'指數碼': 'symbol', '日期': 'datetime'}, inplace=True)
+        elif collection_str == "tsx_property":
+            df.rename(columns={'指數碼': 'symbol', '目前狀態': 'datetime'}, inplace=True)
+        elif collection_str == "Tsecurities_property":
+            df.rename(columns={'證券碼': 'symbol', '目前狀態': 'datetime'}, inplace=True)
+  
+        return df
 
-        keys_list = list(pmart.keys())
-        resent_returns_df = pmart[keys_list[0]]
-        return resent_returns_df
+    def unstack_data(self, df: pd.DataFrame):
+        df_dic = {}
+        column_list = df.columns.to_list()
+        for column in column_list[3:]:
+            unstack_df = df.reset_index().set_index(['datetime', 'symbol'])[column].unstack()
+            df_dic[column] = unstack_df
+        return df_dic
+    
+    # =========================== new function after 2024/03/27 ====================== #
+
+    def get_daily_frequent(self, source_dict: dict, collection_str: str, signal_delay: int= 90, limit = 10000):
+        
+        factor_dict = {}
+
+        tsx_pstage = self.get_data_from_mongo(database_str= 'PSTAGE', collection_str = 'tsx', limit = limit)
+        for_date_dict = self.pdata_pipline(tsx_pstage, collection_str)
+
+        if collection_str == "monthly_revenue":
+            release = '營收發布日'
+        elif collection_str == "financial_report":
+            release = '財報發布日'
+        elif collection_str in ["tsx_property", "securities_property"]:
+            for column in list(source_dict.keys()):
+                data_reindex = source_dict[column].reindex(for_date_dict['指數收盤價'].index, method = 'ffill')
+                factor_dict[column] = data_reindex 
+            return factor_dict
+        
+        for column in list(source_dict.keys()):
+            releaserankpct = source_dict[release].rank(axis=1,pct=True)
+            df_filter = source_dict[column][releaserankpct < signal_delay]
+            df_filter.index = source_dict[release][releaserankpct < signal_delay].max(axis=1)
+            data_reindex = df_filter.reindex(for_date_dict['指數收盤價'].index, method = 'ffill')
+            factor_dict[column] = data_reindex 
+        return factor_dict
+
+    def pmart_pipline(self, source_dict: dict, collection_str: str, signal_delay: int= None):
+        if collection_str not in self.colletion_list:
+            raise ValueError(f"please use one of {self.colletion_list} in collection_str")
+        
+        if type(signal_delay) == int:
+            signal_delay *=  0.01    
+
+        if collection_str in ["margin_trading", "securities_trading_data", "securities_returns", "three_major_investors_activity", "tsx"]:
+            factor_dict = source_dict
+        elif collection_str in ["monthly_revenue"]:
+            list_of_dataframes = list(source_dict.values())
+            first_dataframe = list_of_dataframes[0]
+            limit = len(first_dataframe)*30
+            factor_dict = self.get_daily_frequent(source_dict, collection_str, signal_delay, limit)
+        elif collection_str in ["financial_report"]:
+            list_of_dataframes = list(source_dict.values())
+            first_dataframe = list_of_dataframes[0]
+            limit = len(first_dataframe)*90
+            factor_dict = self.get_daily_frequent(source_dict, collection_str, signal_delay, limit)
+        elif collection_str in ["tsx_property", "securities_property"]:
+            factor_dict = source_dict
+
+        return factor_dict
     
     # def get_revenue_returns(self, review_month: int=1):
     #     today = date.today()
