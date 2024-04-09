868e2fdad1fee015bc0cb29c9060cb1d0fe7bd42
wamoonsky7414 <123857589+wamoonsky7414@users.noreply.github.com>
2024-04-09 16:25:57 +0800

    upup
    upup

--- monitor/tw_monitor.py
+++ monitor/tw_monitor.py
@@ -23,7 +23,7 @@ class TWMarketMonitor(TEJHandler):
         self.client_url = self.tw_monitor_config['mongodb']['client_url']
         self.database_list = self.tw_monitor_config['mongodb']['database']
         self.colletion_list = self.tw_monitor_config['mongodb']['collection']
-        self.company_num = self.tw_monitor_config['limit_for_company_num']
+        self.limit_for_company_num = self.tw_monitor_config['limit_for_company_num']
 
 
     # ==================== data center config ==================== #
@@ -36,7 +36,9 @@ class TWMarketMonitor(TEJHandler):
 
     # ========================= get Pstage data from MongoDB ===================== #
 
-    def get_data_from_mongo(self, database_str:str = 'PSTAGE', collection_str:str = 'financial_report', limit = 10000):
+    def get_data_from_mongo(self, database_str:str = 'PSTAGE', collection_str:str = 'financial_report', limit = None):
+        if limit is None:
+            limit = self.limit_for_company_num
         mongo = MongoClient(self.client_url)
         collection = mongo[database_str][collection_str]
         
@@ -77,7 +79,7 @@ class TWMarketMonitor(TEJHandler):
             df.rename(columns={'指數碼': 'symbol', '日期': 'datetime'}, inplace=True)
         elif collection_str == "tsx_property":
             df.rename(columns={'指數碼': 'symbol', '目前狀態': 'datetime'}, inplace=True)
-        elif collection_str == "Tsecurities_property":
+        elif collection_str == "securities_property":
             df.rename(columns={'證券碼': 'symbol', '目前狀態': 'datetime'}, inplace=True)
   
         return df
@@ -86,15 +88,18 @@ class TWMarketMonitor(TEJHandler):
         df_dic = {}
         column_list = df.columns.to_list()
         for column in column_list[3:]:
-            unstack_df = df.reset_index().set_index(['datetime', 'symbol'])[column].unstack()
+            cleaned_df = df.drop_duplicates(subset=['datetime', 'symbol'], keep='first') # be careful this function will effect other collection or not
+            unstack_df = cleaned_df.reset_index().set_index(['datetime', 'symbol'])[column].unstack()
             df_dic[column] = unstack_df
         return df_dic
 
-    def get_daily_frequent(self, source_dict: dict, collection_str: str, signal_delay= 0.9, limit = 10000):
+    def get_daily_frequent(self, source_dict: dict, collection_str: str, signal_delay= 0.9, limit = None):
+        if limit is None:
+            limit = self.limit_for_company_num
         
         factor_dict = {}
 
-        tsx_pstage = self.get_data_from_mongo(database_str= 'PSTAGE', collection_str = 'tsx', limit = 0)
+        tsx_pstage = self.get_data_from_mongo(database_str= 'PSTAGE', collection_str = 'tsx', limit = limit)
         for_date_dict = self.pdata_pipline(tsx_pstage, 'tsx')
 
         if collection_str == "monthly_revenue":
@@ -123,21 +128,20 @@ class TWMarketMonitor(TEJHandler):
         if collection_str in ["margin_trading", "securities_trading_data", "securities_returns", "three_major_investors_activity", "tsx"]:
             factor_dict = source_dict
         elif collection_str in ["monthly_revenue"]:
-            list_of_dataframes = list(source_dict.values())
-            first_dataframe = list_of_dataframes[0]
-            limit = len(first_dataframe)*30
+            limit = len(source_dict[list(source_dict)[0]].columns)*30
             factor_dict = self.get_daily_frequent(source_dict, collection_str, signal_delay, limit)
         elif collection_str in ["financial_report"]:
-            list_of_dataframes = list(source_dict.values())
-            first_dataframe = list_of_dataframes[0]
-            limit = len(first_dataframe)*90
+            limit = len(source_dict[list(source_dict)[0]].columns)*90
             factor_dict = self.get_daily_frequent(source_dict, collection_str, signal_delay, limit)
         elif collection_str in ["tsx_property", "securities_property"]:
             factor_dict = source_dict
 
         return factor_dict
     
-    def directly_get_pmart(self, collection, signal_delay = 0.9, limit = 500000):
+    def directly_get_pmart(self, collection, signal_delay = 0.9, limit = None):
+        if limit is None:
+            limit = self.limit_for_company_num
+
         pstage = self.get_data_from_mongo('PSTAGE', collection, limit)
         pdata = self.pdata_pipline(pstage, collection)
         pmart = self.pmart_pipline(pdata, collection, signal_delay)
@@ -147,7 +151,7 @@ class TWMarketMonitor(TEJHandler):
     # ======================== get monitor need info ================ #
 
     def get_position_open(self, ticker_list: list) -> pd.DataFrame:
-        ohlcv_dic = self.directly_get_pmart('securities_trading_data', limit=self.company_num)
+        ohlcv_dic = self.directly_get_pmart('securities_trading_data', limit=self.limit_for_company_num)
         recent_open = ohlcv_dic['開盤價']
         return recent_open.loc[:, ticker_list]
 
