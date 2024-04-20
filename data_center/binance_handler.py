6c6d59a4bbb98470b05b5a7bb45f8b48765865e1
wamoonsky7414 <123857589+wamoonsky7414@users.noreply.github.com>
2024-04-20 12:54:56 +0800

    rearrange binance handler
    rearrange binance handler

--- data_center/binance_handler.py
+++ data_center/binance_handler.py
@@ -15,10 +15,14 @@ class BinanceHandHandler(object):
 
         self.data_center_config = self.get_data_center_config()
 
+        self.base_url = self.data_center_config["basic_url"]
+        self.endpoint_list = self.data_center_config["endpoint_list"]
         self.default_symbol = self.data_center_config['default_symbol']
         self.select_symbol = select_symbol or self.default_symbol
+        self.timezone = self.data_center_config["timezone"]
         self.contracttype = self.data_center_config['contracttype']
         self.interval = self.data_center_config['interval']
+        self.start_time = self.data_center_config['since']
 
 
     # ==================== data center config ==================== #
@@ -37,7 +41,7 @@ class BinanceHandHandler(object):
     def get_origin_data(self, target:str):
         if self.contracttype == "PERPETUAL":
             contracttype_file = 'UPERP'
-        file_path = rf'{PROJECT_ROOT}/data/CRYPTO/BINANCE/ORIGIN/{contracttype_file}/{self.interval}/{target}.csv'
+        file_path = rf'{PROJECT_ROOT}/data/CRYPTO/BINANCE/ORIGIN/{contracttype_file}/ohlcv/{self.interval}/{target}.csv'
         df = pd.read_csv(file_path, index_col=0, parse_dates=True)
         return df
     
@@ -73,22 +77,17 @@ class BinanceHandHandler(object):
             factor_df.to_csv(file_path, index=True)
         return 'Finish'
     
-    # ======================= update binance data ===================== #
+    # ======================= update binance ohlcv data ===================== #
 
-    def update_binanace_data_and_arrange_it_to_become_factor(self):
-        BinanceHandHandler().update_data_from_binance()
-        BinanceHandHandler().arrange_data_to_ohlcv_factor()
-        return 'Finish'
+    def update_ohlcv_data_from_binance(self):
 
+        # target_timezone = pytz.timezone(self.timezone)
 
-    def update_data_from_binance(self):
-        target_timezone = pytz.timezone("Asia/Hong_Kong")
         if self.contracttype == "PERPETUAL":
             contracttype_file = 'UPERP'
 
-            target_timezone = pytz.timezone("Asia/Hong_Kong")
             '''
-            This code can be delete after build the other interval
+            This code can be deleted after build the other interval
             '''
             for target in self.select_symbol:
 
@@ -96,20 +95,20 @@ class BinanceHandHandler(object):
                 yesterday_timestamp_ms = int(yesterday.timestamp() * 1000)
                 end_time = yesterday_timestamp_ms
 
-                file_path = rf'{PROJECT_ROOT}/data/CRYPTO/BINANCE/ORIGIN/{contracttype_file}/{self.interval}/{target}.csv'
+                file_path = rf'{PROJECT_ROOT}/data/CRYPTO/BINANCE/ORIGIN/{contracttype_file}/ohlcv/{self.interval}/{target}.csv'
                 try:
                     df = pd.read_csv(file_path)
                     last_time = pd.to_datetime(df.iloc[-1]['datetime'])
-                    print(f"Loading: {target}, Last record time: {last_time}, {target_timezone}")  
+                    print(f"Loading: {target}, Last record time: {last_time}, {self.timezone}")  
                     next_day = last_time + pd.Timedelta(days=1)
-                    start_time = int(pd.Timestamp(next_day, tz=target_timezone).timestamp() * 1000)
+                    start_time = int(pd.Timestamp(next_day, tz=self.timezone).timestamp() * 1000)
 
                 except FileNotFoundError:
                     print(f'Generateing new file for {target} and loading data from Binance' )
                     df = pd.DataFrame(columns=['datetime','open','high','low','close','volume', 'volvalue', 'takerbuy', 'takerbuyvalue']) 
                     os.makedirs(os.path.dirname(file_path), exist_ok=True)
                     df.to_csv(file_path, index=False)
-                    start_time = int(pd.Timestamp(self.data_center_config['since'], tz=target_timezone).timestamp() * 1000)
+                    start_time = int(pd.Timestamp(self.start_time, tz=self.timezone).timestamp() * 1000)
 
                 pair = target
                 ContractType = self.contracttype
@@ -130,8 +129,8 @@ class BinanceHandHandler(object):
                         "endTime": end_time,
                         "limit": limit  # Use the chunk_size for each request
                     }
-                    
-                    url = "https://fapi.binance.com/fapi/v1/continuousKlines"
+
+                    url = self.base_url + self.endpoint_list["continuousklines"]
                     response = requests.get(url, params=params)
                     data = response.json()
                     
@@ -162,6 +161,71 @@ class BinanceHandHandler(object):
                     start_time = int(data[-1][0]) + 1  # Set the new start_time to the next timestamp
                     time.sleep(1)
             print(rf'Finish' '\n')
+
+    def update_binanace_ohlcv_data_and_arrange_it_to_become_factor(self):
+        BinanceHandHandler().update_ohlcv_data_from_binance()
+        BinanceHandHandler().arrange_data_to_ohlcv_factor()
+        return 'Finish'
+
+    # ======================= update binance fundingrate data ===================== #
+
+    def update_fundingrate_data_from_binance(self):
+        url = self.base_url + self.endpoint_list["fundingrate"]
+
+        yesterday = datetime.now() - timedelta(days=1)
+        yesterday_timestamp_ms = int(yesterday.timestamp() * 1000)
+        end_time = yesterday_timestamp_ms
+
+        for target in self.select_symbol:
+
+            file_path = rf'{PROJECT_ROOT}/data/CRYPTO/BINANCE/ORIGIN/UPERP/funding_rate/{target}.csv'
+
+            try:
+                df = pd.read_csv(file_path)
+                last_time = pd.to_datetime(df.iloc[-1]['fundingTime'])
+                print(f"Loading: {target}, Last record time: {last_time}, {self.timezone}")  
+                next_day = last_time + pd.Timedelta(days=1)
+                start_time = int(pd.Timestamp(next_day, tz=self.timezone).timestamp() * 1000)
+
+            except FileNotFoundError:
+                print(f'Generateing new file for {target} and loading data from Binance' )
+                df = pd.DataFrame(columns=['fundingTime', 'fundingRate'])
+                os.makedirs(os.path.dirname(file_path), exist_ok=True)
+                df.to_csv(file_path, index=False)
+                start_time = int(pd.Timestamp(self.start_time, tz=self.timezone).timestamp() * 1000)
+
+            while start_time < end_time:
+                url = self.base_url + self.endpoint_list["fundingrate"]
+                response = requests.get(url, params={
+                    "symbol": target,
+                    "startTime": start_time,
+                    "endTime": end_time,
+                    "limit" : 1000,
+                })
+
+                if response.status_code == 200:
+                    data = response.json()
+
+                    if not data:
+                        break
+
+                    new_df = pd.DataFrame(data)
+
+                    new_df['fundingTime'] = pd.to_datetime(new_df['fundingTime'], unit='ms')#, utc=True)
+                    # its timezone is Asia even I use utc = True ...
+
+                    new_df['fundingTime'] = new_df['fundingTime'].dt.strftime('%Y-%m-%d %H:%M:%S')
+                    new_df = new_df.drop(['symbol', 'markPrice'],axis =1)
+                    # The columns "markPrice" = Close as your timezone
+
+                    df = pd.concat([df, new_df],ignore_index=True)
+                    df.to_csv(file_path, index=False)
+
+                    start_time = int(data[-1]['fundingTime']) + 1
+                    time.sleep(1)
+
+                else:
+                    print("Failed to fetch data: Status code", response.status_code)
 
     # def file_explorer(self):
     #     if self.contracttype == "PERPETUAL":
